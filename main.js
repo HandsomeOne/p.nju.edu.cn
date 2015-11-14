8a2ff1fca201156e924fb6402537309b7e08b226
HandsomeOne <anthony_nju@foxmail.com>
2015-11-14 23:48:24 +0800

    smaller range

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ document.querySelector('#login_button').addEventListener('click', toggle)
 document.querySelector('#logout_button').addEventListener('click', toggle)
 function getTransformedPoint(x, y, cx, cy, r) {
   var d = Math.sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy))
-  var _d = Math.atan(d) * r / (Math.PI / 2)
+  var _d = Math.atan(d / 100) * r / (Math.PI / 2)
   var _x = (x - cx) * _d / d
   var _y = (y - cy) * _d / d
   return { 'x': _x, 'y': _y }
@@ -30,7 +30,7 @@ var cxL = eyeL.getBoundingClientRect().left + 9
 var cyL = eyeL.getBoundingClientRect().top + 9
 var cxR = eyeR.getBoundingClientRect().left + 9
 var cyR = eyeR.getBoundingClientRect().top + 9
-document.body.addEventListener('mousemove', function (e) {
+window.addEventListener('mousemove', function (e) {
   var x = e.clientX
   var y = e.clientY
   eyeL.style.left = 25.8 + getTransformedPoint(x, y, cxL, cyL, 5).x + 'px'
